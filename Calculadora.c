793641a97f0f10e27e64910e81458f98ba812dc2
GabooLml <gabrielrojasmendez@gmail.com>
2023-03-02 22:02:33 -0600

    Prueba

--- Calculadora.c
+++ Calculadora.c
@@ -2,18 +2,15 @@
 #include <stdio.h> //Standard input / output
 
 int main() {
-       int n, i, num, sum = 0;
+       int i, num, sum = 0;
 
-   printf("Ingrese la cantidad de numeros que desea sumar: ");
-   scanf("%d", &n);
-
-   for(i=1; i<=n; i++) {
-      printf("Ingrese el numero %d: ", i);
+   for(i=1; i<=2; i++) {
+      //printf("Ingrese el numero %d: ", i);
       scanf("%d", &num);
       sum += num;
    }
 
-   printf("La suma de los %d numeros ingresados es: %d", n, sum);
+   printf("%d", sum);
 
    return 0;
 }
