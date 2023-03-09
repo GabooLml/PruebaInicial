a09fa673b3c29f81af9b9a16cfc41561323407bf
GabooLml <gabrielrojasmendez@gmail.com>
2023-03-09 11:13:08 -0600

    Prueba

--- Calculadora.c
+++ Calculadora.c
@@ -14,7 +14,7 @@ int main() {
 
    printf("%d\n", sum);
 
-   sum = 0;
+   /*sum = 0;
    num = 0;
    for(i=1; i<=4; i++) {
       //printf("Ingrese el numero %d: ", i);
@@ -28,9 +28,9 @@ int main() {
       //printf("Ingrese el numero %d: ", i);
       scanf("%f", &number);
       suma += number;
-   }
+   }*/
 
-   printf("%.3f\n", suma);
+  // printf("%.3f\n", suma);
 
    return 0;
 }
